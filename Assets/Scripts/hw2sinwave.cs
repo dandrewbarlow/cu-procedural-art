09b5efceb46960ff58f8bb239ac607fd0a8179ad
Andrew <dandrewbarlow@gmail.com>
2021-09-20 13:40:47 -0600

    added class repo as submodule, rename materials, refactor script

--- Assets/Scripts/hw2sinwave.cs
+++ Assets/Scripts/hw2sinwave.cs
@@ -19,16 +19,22 @@ public class hw2sinwave : MonoBehaviour
 
     void updateChildren() {
 
+        foreach(Transform child in this.transform)
+        {
+            foreach (Transform grandchild in child.transform)
+            {
+                float y = Mathf.Sin(counter + grandchild.transform.position.x / 5) * amplitude;
+                Vector3 newPos = new Vector3(grandchild.transform.position.x, y, grandchild.transform.position.z);
+                grandchild.transform.position = newPos;
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float y = Mathf.Sin(counter + pos.x / 5) * amplitude;
-        
-        Vector3 newPos = new Vector3(pos.x, y, pos.z);
-        this.transform.position = newPos;
-
+        updateChildren();
         counter += 0.01f;
     }
 }
