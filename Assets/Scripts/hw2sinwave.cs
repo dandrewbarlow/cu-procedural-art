57dbd029b07d49c130bee4610a97f012fa7cdf4a
Andrew <dandrewbarlow@gmail.com>
2021-09-20 21:30:13 -0600

    work on hw2, restructuring

--- Assets/Scripts/hw2sinwave.cs
+++ Assets/Scripts/hw2sinwave.cs
@@ -2,39 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// creating a sin wave with a bunch of spheres
 public class hw2sinwave : MonoBehaviour
 {
-    public float amplitude;
+    public float amplitude, frequency, offset;
     
+    public bool oscillate;
+
     private float counter;
-    private Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
     {
-        amplitude = 20f;
+        amplitude = 60f;
+        frequency = 31.9f;
         counter = 0f;
-        pos = this.transform.position;
+        offset = 0f;
+        oscillate = true;
     }
 
-    void updateChildren() {
+    Vector3 calculatePosition(Vector3 position) {
+        float y = calculateHeight(position).y;
+        Vector3 newPos = new Vector3(position.x, y / 2, position.z);
+        return newPos;
+    }
 
+    Vector3 calculateHeight(Vector3 position) {
+        float y = Mathf.Sin((counter + position.x / frequency) + offset) * amplitude;
+        return new Vector3(1, y, 1);
+    }
+
+    // so I don't have to select all the objects
+    void updateChildren() {
         foreach(Transform child in this.transform)
         {
-            foreach (Transform grandchild in child.transform)
-            {
-                float y = Mathf.Sin(counter + grandchild.transform.position.x / 5) * amplitude;
-                Vector3 newPos = new Vector3(grandchild.transform.position.x, y, grandchild.transform.position.z);
-                grandchild.transform.position = newPos;
-            }
+            child.transform.localScale = calculateHeight(child.transform.position);
+            child.transform.position = calculatePosition(child.transform.position);
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
         updateChildren();
-        counter += 0.01f;
+        if (oscillate) {
+            counter += Time.deltaTime;
+        }
     }
 }
