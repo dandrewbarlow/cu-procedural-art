c18f1eee9748293e009de80c70fdad2755e748bd
Andrew <dandrewbarlow@gmail.com>
2021-09-27 10:40:11 -0600

    class & restructuring for conflicting filenames

--- Assets/Scripts/in-class/LissajousDrawer.cs
+++ Assets/Scripts/in-class/LissajousDrawer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lissajous : MonoBehaviour
+public class LissajousDrawer : MonoBehaviour
 {
     public float CounterX;
     public float CounterY;
