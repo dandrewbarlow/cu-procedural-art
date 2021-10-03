f70ee1203fe2e878403a438aa992aa830ececf2b
Andrew <dandrewbarlow@gmail.com>
2021-10-03 11:23:14 -0600

    refactor box generation to oop

--- Assets/Scripts/homework/hw3/mondrian.cs
+++ Assets/Scripts/homework/hw3/mondrian.cs
@@ -4,77 +4,130 @@ using UnityEngine;
 
 public class mondrian : MonoBehaviour
 {
-    public Color[] colors = {
-        new Color(0, 0, 0, 0), // black
-        new Color(1f, 0, 0, 0), // red
-        new Color(0, 0, 1f, 0), // blue
-        new Color(1f, 1f, 0, 0), // yellow
-        new Color(1f, 1f, 1f, 0) // white
-    };
 
+    public bool randomize;
     public Transform parent;
-    public Transform root;
+
+    private Box[] boxes;
+
+    // modularize box logic
+    public class Box 
+    {
+
+        public Color[] colors = {
+            new Color(0, 0, 0, 0), // black
+            new Color(1f, 0, 0, 0), // red
+            new Color(0, 0, 1f, 0), // blue
+            new Color(1f, 1f, 0, 0), // yellow
+            new Color(1f, 1f, 1f, 0) // white
+        };
+
+        public Transform root;
+        private Transform colorClone;
+
+        // box constructor
+        public Box(Vector3 position, Vector3 size, Transform parent) 
+        {
+            colorClone = Instantiate(parent, root);
+
+            colorClone.localPosition = position;
+            colorClone.localScale = size;
+            setRandomColor();
+
+            // 👨‍🌾 second generation clones, to create borders on boxes
+            Transform[] inbred = new Transform[4];
+
+            for (int i = 0; i < 4; i++ )
+            {
+                inbred[i] = Instantiate(parent, colorClone);
+                            
+                // black borders
+                inbred[i].GetComponent<Renderer>().material.color = colors[0];
+
+                // ok gotta create some borders w/ math. Probably a better way to do this, but just using index & 
+                    // properties of raising (-1)^index to offset in positive & negative x & y directions
+                // borders on x axis
+                if (i < 2) {
+                    inbred[i].localPosition = new Vector3(
+                        0.45f * Mathf.Pow(-1, i) ,
+                        0,
+                        // we want the borders in front of the boxes
+                        -1
+                    );
+                    inbred[i].localScale = new Vector3(
+                        0.1f,
+                        1,
+                        0
+                    );
+                }
+
+                // borders on y axis
+                if (i >= 2) {
+                    inbred[i].localPosition = new Vector3(
+                        0,
+                        0.45f* Mathf.Pow(-1, i),
+                        // we want the borders in front of the boxes
+                        -1
+                    );
+                    inbred[i].localScale = new Vector3(
+                        1,
+                        0.1f,
+                        0
+                    );
+                }
+            }
+
+        } // END: Box()
+
+        public void setRandomColor() 
+        {
+            Color c = colors[Random.Range(1, 5)];
+            colorClone.GetComponent<Renderer>().material.color = c;
+        }
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 5; i++) {
-            createBox(
+        // amount of boxes
+        int n = 5;
+        boxes = new Box[n];
+
+        for (int i = 0; i < n; i++) 
+        {
+            boxes[i] = new Box(
                 new Vector3(1.5f*i, 1.5f*i, 0),
-                new Vector3(1.5f*i, 1.5f*i, 0.1f)
+                new Vector3(1.5f*i, 1.5f*i, 0.1f),
+                parent
             );
         }
+
+        // banish the unit cube to the shadow realm
+            // shadow realm? I'm sending you to tampa, florida 🐊
+            // https://vm.tiktok.com/ZMRwJtfVh/
+        parent.GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKey("p")) {randomize = true;}
 
-    void createBox(Vector3 position, Vector3 size) {
-        Transform clone = Instantiate(parent, root);
-
-        clone.localPosition = position;
-        clone.localScale = size;
-        clone.GetComponent<Renderer>().material.color = colors[Random.Range(1, 5)];
-
-        // 👨‍🌾 second generation clones, to create borders on boxes
-        Transform[] inbred = new Transform[4];
-        for (int i = 0; i < 4; i++ )
+        if (randomize)
         {
-            inbred[i] = Instantiate(parent, clone);
-                        
-            // black
-            inbred[i].GetComponent<Renderer>().material.color = colors[0];
-
-            // ok gotta create some borders w/ math
-
-            if (i < 2) {
-                inbred[i].localPosition = new Vector3(
-                    clone.localPosition.x + (clone.localScale.x / 2) * Mathf.Pow(-1, i) ,
-                    0,
-                    0
-                );
-                inbred[i].localScale = new Vector3(
-                    parent.localScale.y/10,
-                    1,
-                    0
-                );
-            }
-
-            if (i >= 2) {
-                inbred[i].localPosition = new Vector3(
-                    0,
-                    parent.localPosition.y * Mathf.Pow(-1, i) / i+2,
-                    0
-                );
-                inbred[i].localScale = new Vector3(
-                    1,
-                    parent.localScale.x/10,
-                    0
-                );
-            }
+            randomizeAllColors();
+            randomize = false;
         }
     }
+
+   
+    void randomizeAllColors() 
+        {
+            foreach (Box b in boxes)
+            {
+                b.setRandomColor();
+            }
+        }
+    
 }
